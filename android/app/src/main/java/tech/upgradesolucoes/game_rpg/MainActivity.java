7926ede69ef980dc7ae471338637d5c101ee04ac
André Ramos <andre.sis.tec@hotmail.com>
2024-02-25 21:49:45 -0300

    Incluido configurações para android
    Adicionada configuração para a aplicação iniciar e permanecer na horizontal no android.

--- android/app/src/main/java/tech/upgradesolucoes/game_rpg/MainActivity.java
+++ android/app/src/main/java/tech/upgradesolucoes/game_rpg/MainActivity.java
@@ -1,6 +1,13 @@
 package tech.upgradesolucoes.game_rpg;
 
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
 import io.flutter.embedding.android.FlutterActivity;
 
 public class MainActivity extends FlutterActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+    }
 }
